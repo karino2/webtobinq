6534ca62f4f673064cd18239410094b9fb4267b8
mumurik <mumurik@localhost>
2011-08-27 13:44:42 +0900

    Add JUnit test.

---
+++ src/test/java/com/appspot/WebTobinQ/client/QInterpreterTest.java
@@ -0,0 +1,26 @@
+package com.appspot.WebTobinQ.client;
+
+import static junit.framework.Assert.*;
+
+import org.junit.Test;
+
+
+public class QInterpreterTest {
+	public class ConsoleForTest implements Writable
+	{
+		public StringBuffer Result = new StringBuffer();
+		public void write(CharSequence cs) {
+			Result.append(cs);
+		}
+	}
+	@Test
+	public void test_hello()
+	{
+		ConsoleForTest console = new ConsoleForTest();
+		QInterpreter intp = new QInterpreter(console);
+		intp.eval("x<-c(1,2,3)\n");
+		System.out.println(console.Result.toString());
+		assertNotNull(intp);
+	}
+
+}
